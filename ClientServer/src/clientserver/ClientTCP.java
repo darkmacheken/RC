9b7ada28818832cebd01373af63329ac727e9e87
duartegalvao <duartegalvao@tecnico.ulisboa.pt>
2017-09-28 13:28:05 +0100

    AUTISMO DO  L I O N A R D E

--- ClientServer/src/clientserver/ClientTCP.java
+++ ClientServer/src/clientserver/ClientTCP.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.UnknownHostException;
-import exceptions.ClientException;
+import clientserver.exceptions.ClientException;
 
 
 public class ClientTCP {
