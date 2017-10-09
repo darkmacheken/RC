86d80d9ddb7a7d547884aca2f33921b6e4bc7161
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-10-09 17:58:35 +0100

    ProtocolCsClient halfway

---
+++ CentralServer/src/centralserver/protocols/ProtocolCsClient.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package centralserver.protocols;
+
+import centralserver.processing.Request;
+import static java.lang.Integer.max;
+
+/**
+ * Protocol beteween CS-Client, creates de messages to be sent and
+ * confirms the protocol.
+ * @author Pedro Daniel
+ */
+public class ProtocolCsClient {
+
+    /**
+     * Constructor default
+     */
+    public ProtocolCsClient(){}
+    
+    /**
+     * Receive the string from the connection 
+     * @param sentence
+     * @return Request
+     */
+    public Request receive(String sentence){
+        //remove last char from sentence (it should be '\n' from protocol)
+        sentence = sentence.substring(0, max(0,sentence.length()-1));
+        
+        //split sentence by space into array
+        String[] splitedSentence = sentence.split(" ");
+        
+        if()
+    }
+    
+}
