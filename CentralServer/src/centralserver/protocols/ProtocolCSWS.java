fdc77fa58f7e41db0a5747db1de58023bb5241d7
leo-epi <leonardo.epifanio@hotmail.com>
2017-10-10 18:57:38 +0100

    protocolCSWS

--- CentralServer/src/centralserver/protocols/ProtocolCSWS.java
+++ CentralServer/src/centralserver/protocols/ProtocolCSWS.java
@@ -10,5 +10,26 @@ package centralserver.protocols;
  * @author duartegalvao
  */
 public class ProtocolCSWS {
-    
-}
+    private final String _nameAdress;
+    private final String _iP;
+    private final int _port;
+
+    /**
+    *
+    * @param nameAdress
+    * @param iP
+    * @param port
+    */
+    public ProtocolCSClient(String nameAdress, String iP, int port) {
+        _nameAdress = nameAdress;
+        _iP = iP;
+        _port = port;
+    }
+
+    public Report receive(String sentence) {
+
+    }
+
+    public String send(RequestToWS request) {
+
+    }
