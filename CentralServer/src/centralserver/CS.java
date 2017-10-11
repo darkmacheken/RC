bb545dc97624e86da27d23e5c51cd726cbb0e672
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-10-11 15:43:46 +0100

    Serverudp run

--- CentralServer/src/centralserver/CS.java
+++ CentralServer/src/centralserver/CS.java
@@ -6,8 +6,10 @@
 package centralserver;
 
 import centralserver.connection.ServerTCP;
+import centralserver.connection.ServerUDP;
 import centralserver.exceptions.ConnectionException;
 import centralserver.threads.ServerTCPThread;
+import centralserver.threads.ServerUDPThread;
 
 /**
  *
@@ -36,11 +38,18 @@ public class CS {
         
         try {
             WSList list = new WSList();
+            //create serverTCP and run thread
             ServerTCP server = new ServerTCP(cSPort);
-            System.out.println("Server created.");
-            ServerTCPThread thread = new ServerTCPThread(list, server);
-            thread.start();
-            thread.join();
+            System.out.println("Server TCP created.");
+            ServerTCPThread threadTCP = new ServerTCPThread(list, server);
+            threadTCP.start();
+            //create serverUDP and run thread
+            ServerUDP serverUDP = new ServerUDP(cSPort);
+            System.out.println("Server UDP created.");
+            ServerUDPThread threadUDP = new ServerUDPThread(list, serverUDP);
+            threadUDP.start();
+            
+            threadTCP.join();
         }
         catch (ConnectionException e) {
             System.out.println(e.getErrorDescription());
