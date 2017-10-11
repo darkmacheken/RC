f3e976fa632bdf818993ae7a204cf7b8903a7695
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-10-11 14:48:58 +0100

    ProtocolWSCS and ServerUDPThread done.

--- CentralServer/src/centralserver/protocols/ProtocolWSCS.java
+++ CentralServer/src/centralserver/protocols/ProtocolWSCS.java
@@ -5,53 +5,94 @@
  */
 package centralserver.protocols;
 
-import centralserver.processing.report.Report;
-import centralserver.processing.report.ReportError;
-import centralserver.processing.report.ReportOk;
-import centralserver.processing.request.RequestToWS;
+import centralserver.ConnectAddress;
+import centralserver.WSList;
 import static java.lang.Integer.max;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
- * @author duartegalvao
+ * @author Pedro Daniel
  */
 public class ProtocolWSCS {
-    private final String _nameAdress;
-    private final String _iP;
-    private final int _port;
+    private WSList _list;
 
-    /**
-    *
-    * @param nameAdress
-    * @param iP
-    * @param port
-    */
-    public ProtocolWSCS(String nameAdress, String iP, int port) {
-        _nameAdress = nameAdress;
-        _iP = iP;
-        _port = port;
+    public ProtocolWSCS(WSList _list) {
+        this._list = _list;
     }
-
-    public Report receive(String sentence) {
-      //remove last char from sentence (it should be '\n' from protocol)
-      sentence = sentence.substring(0, max(0,sentence.length()-1));
-
-      //split sentence by space into array
-      String[] splitedCommand = sentence.split(" ", 2);
-
-      if (splitedCommand.length != 0) {
-          return new ReportError(_nameAdress, _iP, _port, "ERR");
-      }
-      return null;
-    }
-
-    /**
-     *
-     * @param request
-     * @return
-     */
-    public String send(RequestToWS request) {
-        int size = request.getFile().length();
-        return "WRQ" + request.getpTC() + request.getFileName() + size + request.getFile() + "\n";
+        
+    public String process(String received){
+        //remove last char from sentence (it should be '\n' from protocol)
+        received = received.substring(0, max(0,received.length()-1));
+        
+        String[] splitedReceived = received.split(" ");
+        int splitedReceivedLength = splitedReceived.length;
+        
+        if(splitedReceived.length == 0)
+            return "ERR\n";
+        else if(splitedReceived[0].equals("REG")){
+            if(splitedReceived.length < 4)
+                return "RAK ERR\n";
+            
+            //check portWS
+            int portWS;
+            try{
+                portWS = Integer.parseInt(splitedReceived[splitedReceivedLength - 1]);
+            }
+            catch(NumberFormatException e){
+                return "RAK ERR\n";
+            }
+            
+            //get ip
+            String ipWS = splitedReceived[splitedReceivedLength - 2];
+            
+            //get PTC's and check each one if it has 3 chars
+            List<String> ptcs =  new LinkedList<String>();
+            
+            for(int i = 1; i < splitedReceivedLength - 2; i++){
+                String ptc = splitedReceived[i];
+                if(ptc.length() != 3)
+                    return "RAK ERR\n";
+                ptcs.add(ptc);
+            }
+            
+            //Transform list ptcs do array
+            String[] ptcsArray = (String[]) ptcs.toArray();
+            
+            //create ConnectAddress
+            ConnectAddress addressWS = new ConnectAddress(ipWS, portWS);
+            
+            //add addressWS and ptcsArray to the public list
+            _list.addIP(ptcsArray, addressWS);
+            
+            return "RAK OK\n";
+        }
+        else if(splitedReceived[0].equals("UNR")){
+            if(splitedReceivedLength != 3)
+                return "UAK ERR\n";
+            
+            //check portWS
+            int portWS;
+            try{
+                portWS = Integer.parseInt(splitedReceived[2]);
+            }
+            catch(NumberFormatException e){
+                return "RAK ERR\n";
+            }
+            
+            //get ip
+            String ipWS = splitedReceived[1];
+            
+            //create ConnectAddress
+            ConnectAddress addressWS = new ConnectAddress(ipWS, portWS);
+            
+            //remove addressWS
+            _list.removeIP(addressWS);
+            
+            return "UAK OK\n";
+        }
+            
+        return "ERR\n";
     }
 }
--- CentralServer/src/centralserver/threads/ServerUDPThread.java
+++ CentralServer/src/centralserver/threads/ServerUDPThread.java
@@ -6,17 +6,18 @@
 package centralserver.threads;
 
 import centralserver.WSList;
-import centralserver.connection.ConnectionUDP;
 import centralserver.connection.ServerUDP;
 import centralserver.exceptions.ConnectionException;
+import centralserver.protocols.ProtocolWSCS;
 
 /**
  *
- * @author duartegalvao
+ * @author Pedro Daniel
  */
 public class ServerUDPThread extends Thread {
     WSList _list;
     ServerUDP _server;
+    ProtocolWSCS _protocol;
     
     /**
      *
@@ -26,16 +27,16 @@ public class ServerUDPThread extends Thread {
     public ServerUDPThread(WSList list, ServerUDP server) {
         _list = list;
         _server = server;
+        _protocol = new ProtocolWSCS(_list);
     }
     
     @Override
     public void run() {
         while (true) {
             try {
-                ConnectionUDP connection = _server.acceptSocket();
-                ServerUDPConnectionThread connectionThread = new ServerUDPConnectionThread(connection, _list);
-                System.out.println("Connected to client: " + connection.getName() + ":" + connection.getPort());
-                connectionThread.start();
+                String received = _server.receive();
+                String toSend = _protocol.process(received);
+                _server.send(toSend);
             }
             catch (ConnectionException e) {
                 System.err.println(e.getErrorDescription());
